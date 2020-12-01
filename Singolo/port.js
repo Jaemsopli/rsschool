bda6b8f9a6d722af76cafa31df3313b69c8bf602
Jaemsopli <Babby07@mail.ru>
2020-12-01 12:00:11 +0300

    unit: Add a link to Markdown & Git

--- Singolo/port.js
+++ Singolo/port.js
@@ -1,18 +1,18 @@
-const slide = document.querySelector('.iphone');
-const slideImg = document.querySelectorAll('.iphone img');
+const slide = document.querySelector('.flex-size');
+const slideImg = document.querySelectorAll('.flex-size img');
 
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
 
 let counter = 1;
 const size = slideImg[0].clientWidth;
-slide.style.transform = 'translateX(' + ( -size * counter ) + 'px)';
+slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 nextBtn.addEventListener('click', ()=> {
-if (counter >= slideImg.length -1) return;
+if (counter >= slideImg.length - 1) return;
 slide.style.transition = "transform 0.4s ease-in-out";
 counter++;
-slide.style.transform = 'translateX(' + ( -size * counter ) + 'px)';
+slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 });
 
 prevBtn.addEventListener('click', ()=> {
@@ -25,12 +25,12 @@ slide.style.transform = 'translateX(' + ( -size * counter ) + 'px)';
 slide.addEventListener('transitionend', ()=> {
 if (slideImg[counter].id === 'last') {
 slide.style.transition = "none";
-counter = slideImg.length - 2;
-slide.style.transform = 'translateX(' + ( -size * counter ) + 'px)';
+counter = slideImg.length -2;
+slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 }
 if (slideImg[counter].id === 'first') {
 slide.style.transition = "none";
 counter = slideImg.length - counter;
-slide.style.transform = 'translateX(' + ( -size * counter ) + 'px)';
+slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 }
 });
